3b914bfd9e595e9dc1c6a0175242c345c6e1a38c
YassinEldeeb <yassineldeeb94@gmail.com>
2021-11-29 21:27:44 +0200

    implement methods to structs

--- rectangle/src/main.rs
+++ rectangle/src/main.rs
@@ -4,17 +4,19 @@ struct Rectangle {
     height: u32,
 }
 
+impl Rectangle {
+    fn area(&self) -> u32 {
+        self.width * self.height
+    }
+}
+
 fn main() {
     let rect = Rectangle {
-        width: 30,
-        height: 50,
+        width: 5,
+        height: 3,
     };
 
     dbg!(&rect);
 
-    println!("The Area of the Rectangle is {}", area(&rect));
-}
-
-fn area(rectangle: &Rectangle) -> u32 {
-    rectangle.width * rectangle.height
+    println!("The Area of the Rectangle is {}", rect.area());
 }
