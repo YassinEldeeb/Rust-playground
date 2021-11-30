539217a1d05a3bdf3814fe09bd9182f9af38fb25
YassinEldeeb <yassineldeeb94@gmail.com>
2021-11-30 07:20:06 +0200

    associated functions

--- rectangle/src/main.rs
+++ rectangle/src/main.rs
@@ -8,15 +8,33 @@ impl Rectangle {
     fn area(&self) -> u32 {
         self.width * self.height
     }
+
+    fn can_hold(&self, rectangle: &Rectangle) -> bool {
+        self.width > rectangle.width && self.height > rectangle.height
+    }
+
+    fn square(size: u32) -> Rectangle {
+        Rectangle {
+            width: size,
+            height: size,
+        }
+    }
 }
 
 fn main() {
-    let rect = Rectangle {
+    let rect1 = Rectangle {
         width: 5,
         height: 3,
     };
 
-    dbg!(&rect);
+    let rect2 = Rectangle {
+        width: 4,
+        height: 2,
+    };
 
-    println!("The Area of the Rectangle is {}", rect.area());
+    dbg!(&rect1);
+
+    println!("The Area of the Rectangle is {}", rect1.area());
+    println!("Can Hold {}", rect1.can_hold(&rect2));
+    println!("Associated Function {:#?}", Rectangle::square(2));
 }
