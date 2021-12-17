2a2548cbff336e8242416d43ac8e5407771208a0
YassinEldeeb <yassineldeeb94@gmail.com>
2021-12-17 20:49:18 +0200

    using closures to handle errors

--- error-handeling/src/main.rs
+++ error-handeling/src/main.rs
@@ -18,20 +18,32 @@ use std::io::ErrorKind;
 // }
 
 fn main() {
-    let r = File::open("./hello.txt");
+    // Nested match expressions
+    // let r = File::open("hello.txt");
 
-    let r = match r {
-        Ok(file) => file,
-        Err(error) => match error.kind() {
-            ErrorKind::NotFound => match File::create("hello.txt") {
-                Ok(file) => file,
-                Err(error) => panic!("Couldn't create 'hello.txt' file: {}", error),
-            },
-            other_error => {
-                panic!("Problem openning the file: {:?}", other_error)
-            }
-        },
-    };
+    // let r = match r {
+    //     Ok(file) => file,
+    //     Err(error) => match error.kind() {
+    //         ErrorKind::NotFound => match File::create("hello.txt") {
+    //             Ok(file) => file,
+    //             Err(error) => panic!("Couldn't create 'hello.txt' file: {}", error),
+    //         },
+    //         other_error => {
+    //             panic!("Problem openning the file: {:?}", other_error)
+    //         }
+    //     },
+    // };
+
+    // Closures
+    let r = File::open("hello.txt").unwrap_or_else(|error| {
+        if error.kind() == ErrorKind::NotFound {
+            File::create("hello.txt").unwrap_or_else(|error| {
+                panic!("Couldn't create 'hello.txt' file: {}", error);
+            })
+        } else {
+            panic!("Problem openning the file: {:?}", error)
+        }
+    });
 
     println!("{:?}", r);
 }
