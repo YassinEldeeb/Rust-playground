459bfd5c61dc21c500192899b15475e43b9f855e
YassinEldeeb <yassineldeeb94@gmail.com>
2022-02-10 12:15:38 +0200

    better version of the Cacher struct

--- closures/src/main.rs
+++ closures/src/main.rs
@@ -1,34 +1,38 @@
-use std::{collections::HashMap, thread, time::Duration};
+use std::{collections::HashMap, hash::Hash, thread, time::Duration};
 
-struct Cacher<T>
+struct Cacher<T, U, V>
 where
-    T: Fn(u32) -> u32,
+    T: Fn(U) -> V,
+    U: Hash + Eq + Clone,
+    V: Clone,
 {
-    calculation: T,
-    cached_values: HashMap<u32, u32>,
+    expensive_closure: T,
+    cache: HashMap<U, V>,
 }
 
-impl<T> Cacher<T>
+impl<T, U, V> Cacher<T, U, V>
 where
-    T: Fn(u32) -> u32,
+    T: Fn(U) -> V,
+    U: Hash + Eq + Clone,
+    V: Clone,
 {
-    fn new(calculation: T) -> Self {
+    fn new(closure: T) -> Self {
         Cacher {
-            calculation,
-            cached_values: HashMap::new(),
+            expensive_closure: closure,
+            cache: HashMap::new(),
         }
     }
 
-    fn value(&mut self, arg: u32) -> u32 {
-        let value = self.cached_values.get(&arg);
+    fn value(&mut self, arg: U) -> V {
+        let value = self.cache.get(&arg);
 
         match value {
-            Some(v) => *v,
+            Some(v) => (*v).clone(),
             None => {
-                let v = (self.calculation)(arg);
-                self.cached_values.insert(arg, v);
+                let v = (self.expensive_closure)(arg.clone());
+                self.cache.insert(arg.clone(), v.clone());
 
-                v
+                (self.cache.get(&arg).unwrap()).clone()
             }
         }
     }
@@ -36,45 +40,32 @@ where
 
 fn main() {
     let simulated_user_specified_value = 10;
-    let simulated_random_number = 3;
+    let simulated_random_number = 7;
 
     generate_workout(simulated_user_specified_value, simulated_random_number);
 }
 
 fn generate_workout(intensity: u32, random_number: u32) {
-    let mut expensive_closure = Cacher::new(|num| {
-        println!("slowly calculating...");
+    let mut expensive_colsure = Cacher::new(|num| {
+        println!("calculating slowly...");
         thread::sleep(Duration::from_secs(2));
         num
     });
 
     if intensity < 25 {
-        println!("Today, do {} pushups!", expensive_closure.value(intensity));
-        println!("Next, do {} situps!", expensive_closure.value(intensity));
+        println!(
+            "Today, do {} pushups!",
+            expensive_colsure.value(intensity.clone())
+        );
+        println!("Next, do {} situps!", expensive_colsure.value(intensity));
     } else {
         if random_number == 3 {
             println!("Take a break today! Remember to stay hydrated!");
         } else {
             println!(
                 "Today, run for {} minutes!",
-                expensive_closure.value(intensity)
+                expensive_colsure.value(intensity)
             );
         }
-    }
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn call_with_different_values() {
-        let mut c = Cacher::new(|a| a);
-
-        let v1 = c.value(1);
-        let v2 = c.value(2);
-
-        assert_eq!(v1, 1);
-        assert_eq!(v2, 2);
     }
 }
