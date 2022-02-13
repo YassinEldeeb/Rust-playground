0919219916f9a50e140f1ef152e0c553a54da5bb
YassinEldeeb <yassineldeeb94@gmail.com>
2022-02-13 09:57:28 +0200

    Box<T>

---
+++ smart pointers/boxes/src/main.rs
@@ -0,0 +1,15 @@
+#[derive(Debug)]
+enum List {
+    Cons(i32, Box<List>),
+    Nil,
+}
+
+use List::{Cons, Nil};
+
+fn main() {
+    let list = Cons(1, Box::new(Cons(2, Box::new(Cons(3, Box::new(Nil))))));
+
+    let x = Box::new(5);
+
+    println!("{:#?}", list);
+}
